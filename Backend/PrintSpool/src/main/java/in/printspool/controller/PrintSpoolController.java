c810ad7e3a2d633aa638ea197becb175f4af2a13
Roland Ortega <rolandandresortega@gmail.com>
2022-12-07 19:02:33 -0500

    Print Spool dinámico completo, el CSV queda para el Front

--- Backend/PrintSpool/src/main/java/in/printspool/controller/PrintSpoolController.java
+++ Backend/PrintSpool/src/main/java/in/printspool/controller/PrintSpoolController.java
@@ -1,6 +1,6 @@
 package in.printspool.controller;
 
-import java.net.URI;
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import in.printspool.model.PrintSpool;
 import in.printspool.model.SpoolConfig;
-import in.printspool.model.Stratum;
 import in.printspool.repository.PrintSpoolRepository;
 
 @RestController
@@ -23,71 +26,187 @@ public class PrintSpoolController {
 	private PrintSpoolRepository printSpoolRepository;
 	
 	@GetMapping(value="/api/spool")
-	public ResponseEntity<List<PrintSpool>> getPrintSpool(@RequestBody SpoolConfig spoolConfig) {
+	public ResponseEntity<byte[]> getPrintSpool(@RequestBody SpoolConfig spoolConfig) {
 		SpoolConfig x = spoolConfig;
 		if (x.isStratum() && x.isAvgConsumption() && x.isLastConsumption()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolSAL(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolSAL(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				
+				return new ResponseEntity<byte[]>(jsonByte, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
 			
 		} else if (x.isStratum() && x.isAvgConsumption()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolSA(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolSA(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("stratum");
+		                edit.remove("avgConsumption");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
-			
+
 		} else if (x.isStratum() && x.isLastConsumption()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolSL(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolSL(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("stratum");
+		                edit.remove("last");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
-			
+
 		} else if (x.isAvgConsumption() && x.isLastConsumption()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolAL(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolAL(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("avgConsumption");
+		                edit.remove("last");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
-			
+
 		} else if (x.isStratum()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolS(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolS(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("stratum");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
-			
+
 		} else if (x.isAvgConsumption()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolA(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolA(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("avgConsumption");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
-			
+
 		} else if (x.isLastConsumption()) {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpoolL(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpoolL(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("last");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
-			
+
 		} else {
-			
 			try {
-				return ResponseEntity.ok(printSpoolRepository.getPrintSpool(spoolConfig));
+				
+				List<PrintSpool> printSpool = printSpoolRepository.getPrintSpool(spoolConfig);
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = objectMapper.writeValueAsString(printSpool);
+				byte[] jsonByte = json.getBytes();
+				JsonNode root = objectMapper.readTree(jsonByte);
+		        for (JsonNode jsonNode : root) {
+		            if (jsonNode instanceof ObjectNode) {
+		                ObjectNode edit = (ObjectNode) jsonNode;
+		                edit.remove("stratum");
+		                edit.remove("avgConsumption");
+		                edit.remove("last");
+		            }
+		        }
+		        String jsonFinal = objectMapper.writeValueAsString(root);
+		        byte[] jsonByteFinal = jsonFinal.getBytes();
+				
+				
+				return new ResponseEntity<byte[]>(jsonByteFinal, HttpStatus.OK);
 			} catch (Exception e) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
+
 		}
 	}
 }
